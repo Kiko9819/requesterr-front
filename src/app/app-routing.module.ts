c30a915a3974d2bc5f0f30c74f41ad119e12c6f0
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-02-18 23:41:57 +0200

    Setup routing on the root module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
+import { NonAuthGuard } from './services/non-auth.guard';
 
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
-    // canLoad should be here
-  }
+    canLoad: [NonAuthGuard]
+  },
+  {
+		path: '',
+		loadChildren: () => import('./main/main.module').then(m => m.MainModule),
+		canLoad: [AuthGuard]
+	},
+	{
+		path: '**',
+		redirectTo: '/'
+	}
+
 ];
 
 @NgModule({
