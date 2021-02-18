63ebf794cddb014f0ee7b12543873778df12dfca
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-02-18 23:36:35 +0200

    Add auth api service which makes the calls to the API

--- src/app/auth/services/auth-api.service.ts
+++ src/app/auth/services/auth-api.service.ts
@@ -1,7 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
+import { AuthService } from './auth.service';
+import { AuthState } from './auth.state';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +12,51 @@ import { environment } from '../../../environments/environment';
 export class AuthApiService {
 
   readonly url = `${environment.apiUrl}/auth`;
-  readonly loginUrl = `${this.url}/signin`;
+  readonly signInUrl = `${this.url}/signin`;
+  readonly signOutUrl = `${this.url}/signout`;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService,
+    private authState: AuthState
+    ) {
   }
 
   // TODO: FIX ANY
   login$(user: any): Observable<any> {
-    console.log(this.loginUrl);
-    return this.httpClient.post(this.loginUrl, {email: user.email, password: user.password});
+    console.log(this.signInUrl);
+    return this.httpClient.post(this.signInUrl, {email: user.email, password: user.password}).pipe(
+      tap((response: any) => {
+        this.authService.accessToken = response.access_token;
+				this.authService.refreshToken = response.refresh_token;
+      }),
+      tap(() => {
+				this.authService.navigateToMain();
+				this.authService.removeRequestedPath();
+      }),
+      catchError((response: HttpErrorResponse) => {
+        return throwError(response.error);
+      })
+    );
   }
+
+  // make any => login response
+  refreshToken$(): Observable<any> {
+		const body = new FormData();
+		body.append('refresh_token', this.authService.refreshToken);
+
+		return this.httpClient.post<any>(this.signInUrl, body);
+  }
+  
+  logout$(): Observable<void> {
+    // TODO: probably handle form data on the api side
+		// const body = new FormData();
+		// body.append('access_token', this.authService.accessToken);
+
+		return this.httpClient.post<void>(this.signOutUrl, {access_token: this.authService.accessToken}).pipe(
+			tap(() => this.authService.clearSession())
+		);
+	}
+
+
 }
