9f909729cbe75af30b9a9dabfcedd212b75d320c
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-02-18 23:41:30 +0200

    Change access token name

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,29 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
-import { catchError, filter, take, switchMap } from "rxjs/operators";
-import { Observable, throwError } from 'rxjs';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from "rxjs/operators";
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<Object>, next: HttpHandler) {
-    console.log('Interception In Progress'); // SECTION 1
-    const token: string = localStorage.getItem('token');
+    const token: string = localStorage.getItem('access_token');
     req = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + token) });
     req = req.clone({ headers: req.headers.set('Content-Type', 'application/json') });
     req = req.clone({ headers: req.headers.set('Accept', 'application/json') });
 
     return next.handle(req)
-        .pipe(
-           catchError((error: HttpErrorResponse) => {
-                // 401 UNAUTHORIZED - SECTION 2
-                if (error && error.status === 401) {
-                    console.log('ERROR 401 UNAUTHORIZED');
-                }
-                const err = error.error.message || error.statusText;
-                return throwError(error);
-           })
-        );
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          // 401 UNAUTHORIZED - SECTION 2
+          if (error && error.status === 401) {
+            console.log('ERROR 401 UNAUTHORIZED');
+          }
+          const err = error.error.message || error.statusText;
+          return throwError(error);
+        })
+      );
   }
 }
