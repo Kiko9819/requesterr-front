797fe6d28338ed7c1db1b624d7fbdd1088af5ee7
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-03-05 00:27:54 +0200

    Add my profile module

---
+++ src/app/main/my-profile/my-profile.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyProfileComponent } from './my-profile.component';
+import { MyProfileRoutingModule } from './my-profile-routing.module';
+import { MyProfileFormComponent } from './components/my-profile-form/my-profile-form.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzInputModule, NzButtonModule, NzTypographyModule } from 'ng-zorro-antd';
+
+@NgModule({
+  declarations: [MyProfileComponent, MyProfileFormComponent],
+  imports: [
+    CommonModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzTypographyModule,
+    ReactiveFormsModule,
+    MyProfileRoutingModule
+  ]
+})
+export class MyProfileModule { }
