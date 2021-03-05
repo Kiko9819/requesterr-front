b4c2e8066a024b6ed587cfd4bcbfd67758c74b0c
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-03-05 22:50:22 +0200

    Implement update my-profile

--- src/app/main/my-profile/components/my-profile-form/my-profile-form.component.ts
+++ src/app/main/my-profile/components/my-profile-form/my-profile-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { UsersFacade } from 'src/app/main/services/users.facade';
+import { UsersState } from 'src/app/main/services/users.state';
 
 @Component({
   selector: 'app-my-profile-form',
@@ -10,21 +14,38 @@ export class MyProfileFormComponent implements OnInit {
 
   @Input() user;
 
+  loading$: Observable<boolean>;
+
   formGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private usersFacade: UsersFacade,
+    private usersState: UsersState) { 
+      this.loading$ = this.usersState.getMyProfileLoading$();
+    }
 
   ngOnInit(): void {
-    console.log(this.user.email)
-
     this.formGroup = this.formBuilder.group({
-      email: [],
-      password: []
+      name: [this.user.name],
+      email: [this.user.email, [Validators.email]],
     })
   }
 
+  // maybe create an abstraction that contains this logic and only call the needed api methods after calling super.
   submitForm(): void {
-    console.log("Submitting form")
+    for (const i in this.formGroup.controls) {
+      this.formGroup.controls[i].markAsDirty();
+      this.formGroup.controls[i].updateValueAndValidity();
+    }
+
+    if (this.formGroup.invalid) {
+      return;
+    }
+
+    this.usersFacade.update$(this.formGroup.value, this.user.id).pipe(
+      take(1)
+    ).subscribe();
   }
 
 }
