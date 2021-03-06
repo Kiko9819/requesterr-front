8746acd174f817ad6fa1c9ce0056608b16fa0f83
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-03-06 15:44:36 +0200

    Cleanup

--- src/app/auth/services/auth-api.service.ts
+++ src/app/auth/services/auth-api.service.ts
@@ -2,12 +2,10 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
-import { Tokens } from 'src/app/shared/enums/tokens.enum';
 import { environment } from '../../../environments/environment';
 import { IUserLoginDTO } from '../models/IUserLoginDTO.interface';
 import { IUserLoginResponseDTO } from '../models/IUserLoginResponseDTO.interface';
 import { AuthService } from './auth.service';
-import { AuthState } from './auth.state';
 
 @Injectable({
   providedIn: 'root'
@@ -20,20 +18,19 @@ export class AuthApiService {
 
   constructor(
     private httpClient: HttpClient,
-    private authService: AuthService,
-    private authState: AuthState
-    ) {
+    private authService: AuthService
+  ) {
   }
 
   login$(user: IUserLoginDTO): Observable<IUserLoginResponseDTO> {
-    return this.httpClient.post<IUserLoginResponseDTO>(this.signInUrl, {email: user.email, password: user.password}).pipe(
+    return this.httpClient.post<IUserLoginResponseDTO>(this.signInUrl, { email: user.email, password: user.password }).pipe(
       tap((response: IUserLoginResponseDTO) => {
         this.authService.accessToken = response.access_token;
-				this.authService.refreshToken = response.refresh_token;
+        this.authService.refreshToken = response.refresh_token;
       }),
       tap(() => {
-				this.authService.navigateToMain();
-				this.authService.removeRequestedPath();
+        this.authService.navigateToMain();
+        this.authService.removeRequestedPath();
       }),
       catchError((response: HttpErrorResponse) => {
         return throwError(response.error);
@@ -43,21 +40,12 @@ export class AuthApiService {
 
   // make any => login response
   refreshToken$(): Observable<any> {
-		const body = new FormData();
-		body.append(Tokens.Refresh, this.authService.refreshToken);
-
-		return this.httpClient.post<any>(this.signInUrl, body);
+    return this.httpClient.post<any>(this.signInUrl, { refresh_token: this.authService.refreshToken });
   }
-  
+
   logout$(): Observable<void> {
-    // TODO: probably handle form data on the api side
-		// const body = new FormData();
-		// body.append('access_token', this.authService.accessToken);
-
-		return this.httpClient.post<void>(this.signOutUrl, {access_token: this.authService.accessToken}).pipe(
-			tap(() => this.authService.clearSession())
-		);
-	}
-
-
+    return this.httpClient.post<void>(this.signOutUrl, { access_token: this.authService.accessToken }).pipe(
+      tap(() => this.authService.clearSession())
+    );
+  }
 }
